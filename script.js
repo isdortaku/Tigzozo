428bb525394587a4e5c1caa82082e6464e9160dd
Isdor Taku <isdortakudzwamunengwa@gmail.com>
2024-05-07 00:52:24 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
